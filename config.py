2a226fb7853636a1b4d79581ca0de272270149ba
Mk700617mk <ssmkmt3@gmail.com>
2025-10-02 19:35:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "25426270"))
 API_HASH = getenv("API_HASH", "2f4948dc659e5af3f87bfc8baba6b921")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", "6107581019"))
+BOT_TOKEN = getenv("BOT_TOKEN", "8400846080:AAF4WW9FjwhFDYUCflX4qkI_l3MFgfqUIfY")
+OWNER_ID = int(getenv("OWNER_ID", "6643337452"))
 MONGO_DB = getenv("MONGO_DB", "")
